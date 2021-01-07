5d5b44062f94a3901f5897b2d03e18e27bafbd9a
VINOTH <VINOTH@DESKTOP-VINOTH>
2021-01-07 20:39:08 +0530

    Exercise 1

---
+++ SecondSmall/src/main/java/co/com/practice/util/SecondSmall/App.java
@@ -0,0 +1,13 @@
+package co.com.practice.util.SecondSmall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
